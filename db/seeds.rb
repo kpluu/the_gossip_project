a7cb18b9bbc07081a26c5feea7944103d4b3f209
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-10-24 18:41:04 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+10.times do |index|
+  c = City.create(name: Faker::Nation.capital_city, zip_code: Faker::Code.nric)
+  u = User.create(first_name: Faker::Name.name, last_name: Faker::Name.last_name, description: Faker::Movie.quote, email: Faker::Internet.email, age: Faker::Number.between(from: 20, to: 50), city_id: index+1)
+end
